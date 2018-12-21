5a8656d1bcc4c5988956945cdeda0dd50feaccc2
Vinny Bass <vinicius@starste.com>
2018-12-21 17:29:18 -0200

    aula 3 modulo 4(METADE)

--- classes.js
+++ classes.js
@@ -1,7 +1,48 @@
 // let = limita o seu uso ao escopo em que foi declarado
 //Função construtora, primeira letra em maiusculo e uso do new
-function SoftwareDeveloper(){
+function SoftwareDeveloper(name){
 	this.favoriteLanguage = 'JavaScript';
+	this.name = name;
 }
 
-let developer = new SoftwareDeveloper();
+let developer = new SoftwareDeveloper('Vinicius');
+console.log(developer);
+// SoftwareDeveloper { favoriteLanguage: 'JavaScript', name: 'Vinicius' }
+
+//Criando função construtora, nela this vai ter o valor do objeto instanciado x this = taylor
+function Hero(name, role){
+	this.name = name;
+	this.role = role;
+
+	this.introduce = function(){
+		console.log('My name is ${this.name} and I am a ${this.role}.');
+	}
+}
+
+//Instanciando o objeto Hero
+
+const taylor = new Hero('Taylor','Mother');
+const tite = new Hero('Tite','coach');
+
+taylor.name;//Resulta em Taylor
+taylor.introduce();//Resulta na frse com o name and the role
+
+//Formas de invocar uma função
+function multiply(n1,n2){
+	return n1 * n2;
+}
+
+multiply.call(window,n1,n2);//O primeiro parametro deve ser o que queremos definir como this
+
+const mockingbird = {
+  title: 'O Sol é Para Todos',
+  describe: function () {
+    console.log(`${this.title} é um romance clássico`);
+  }
+};
+
+const pride = {
+  title: 'Orgulho e Preconceito'
+};
+
+mockingbird.describe.call(pride); // Ira retornar 'Orgulho e Preconceito é um romance clássico'
